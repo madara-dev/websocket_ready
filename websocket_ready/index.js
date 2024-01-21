3623a49ebf8fb85f8a36cf557e50f98378f8009a
Madara_Dev <87358674+madara-dev@users.noreply.github.com>
2024-01-21 16:31:48 +0530

    Add files via upload

---
+++ websocket_ready/index.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const { Server } = require('socket.io')
+const http = require("http")
+const cors = require("cors")
+const { userModel, addressFinder, attribute_adder } = require('./mongo')
+var bodyParser = require("body-parser");
+
+// initation
+const routes = express.Router()
+
+const app = express();
+app.use(cors())
+
+
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  },
+});
+
+
+
+io.on("connection", (socket) => {
+  // console.log(`user connected: ${socket.id}`)
+
+  socket.on('address', async (e) => {
+
+    // console.log(typeof e.email)
+
+    // console.log('logged sei address is: ' +e);  
+    a = await addressFinder(e)
+
+    // console.log(a)
+
+    if (a === false) {
+      const user = userModel({
+        address: e,
+        twitter: '',
+        discord: '',
+        quote_link: '',
+        zily_username: ''
+      })
+
+      await user.save()
+
+      socket.emit('data', await addressFinder(e))
+
+    } else{
+
+      socket.emit('data', a)
+      
+    }
+
+
+  })
+
+
+  socket.on('twitter', async (e)=>{
+    // console.log('twiter '+e);
+   a = await attribute_adder(e)
+
+   socket.emit('socialConformation', a)
+   
+  })
+
+  socket.on('discord', async (e)=>{
+    attribute_adder(e)
+  })
+
+  socket.on('quote_link', async (e)=>{
+    attribute_adder(e)
+
+  })
+
+  socket.on('zily_username', async (e)=>{
+    attribute_adder(e)
+  })
+
+})
+
+
+app.use(routes)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+server.listen(3001, () => {
+  console.log("server is running on 3001")
+})
+
---
+++ websocket_ready/mongo.js
@@ -0,0 +1,84 @@
+const mongoose = require('mongoose');
+// const nanofuck = require('./nano')
+mongoose.connect('mongodb://127.0.0.1:27017/WebSocket');
+// mongoose.connect('mongodb://127.0.0.1:27017/clickCounter');
+
+
+
+const UserSchema = new mongoose.Schema({
+  address: String,
+  twitter: String,
+  discord: String,
+  quote_link: String,
+  zily_username: String
+});
+const userModel = mongoose.model('users', UserSchema);
+
+
+async function addressFinder(param) {
+  const address = await (await userModel.find({ address: param }).exec()).pop()
+
+  if (address) {
+    return address
+  } else {
+    return false
+  }
+
+}
+
+async function attribute_adder(param) {
+
+  const details = await JSON.parse(param)
+  const address = await (await userModel.find({ address: details.address }).exec()).pop()
+  // console.log(details);
+  if (address) {
+
+    if (details.task === "twitter") {
+      try {
+        await userModel.updateOne({ address: details.address }, { twitter: details.twitter_id })
+        return { twitter: "success" }
+        
+      } catch (error) {
+        return {error}
+      }
+
+    } else if (details.task === "discord") {
+      try {
+        await userModel.updateOne({ address: details.address }, { discord: details.discord_id })
+        
+        return { twitter: "success" }
+      } catch (error) {
+        return {error}
+      }
+
+    } else if (details.task === "quote_link") {
+      try {
+        await userModel.updateOne({ address: details.address }, { quote_link: details.quote_link })
+        return { twitter: "success" }
+        
+      } catch (error) {
+        return {error}
+      }
+
+
+    } else if (details.task === "zily_username") {
+      try {
+        await userModel.updateOne({ address: details.address }, { zily_username: details.zily_username })
+        return { twitter: "success" }
+        
+      } catch (error) {
+        return {error}
+      }
+
+    }
+
+  } else {
+
+    return ""
+
+  }
+
+}
+
+
+module.exports = { userModel, addressFinder, attribute_adder }
